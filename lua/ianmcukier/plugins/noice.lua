660f28dbcd1faad64be922f84da5640fa0082b51
ianmcukier <ianmcukier@gmail.com>
2024-05-28 20:15:14 -0300

    change ui

--- lua/ianmcukier/plugins/noice.lua
+++ lua/ianmcukier/plugins/noice.lua
@@ -1,47 +1,54 @@
 -- lazy.nvim
 return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
-    {
-      "rcarriga/nvim-notify",
-      opts = {
-        top_down = true,
-        stages = "static",
-      }
-    },
-  },
-  config = function()
-    require("noice").setup({
-      cmdline = {
-        view = "cmdline",
-        format = {
-          cmdline = { pattern = "^:", icon = " ", lang = "vim" },
-          search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
-          search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
-        },
-      },
-      lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-        },
-      },
-      -- you can enable a preset for easier configuration
-      presets = {
-        bottom_search = true,         -- classic bottom cmdline for search
-        command_palette = false,      -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = true,        -- add a border to hover docs and signature help
-      },
-    })
-  end
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+		"MunifTanjim/nui.nvim",
+		-- OPTIONAL:
+		--   `nvim-notify` is only needed, if you want to use the notification view.
+		--   If not available, we use `mini` as the fallback
+		{
+			"rcarriga/nvim-notify",
+			opts = {
+				top_down = true,
+				stages = "static",
+				max_height = 4,
+				max_width = 50,
+			},
+		},
+	},
+	config = function()
+		require("noice").setup({
+			cmdline = {
+				view = "cmdline",
+				format = {
+					cmdline = { pattern = "^:", icon = " ", lang = "vim" },
+					search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+					search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+				},
+			},
+			lsp = {
+				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+				},
+				{
+					progress = {
+						enabled = true,
+					},
+				},
+			},
+			-- you can enable a preset for easier configuration
+			presets = {
+				bottom_search = true, -- classic bottom cmdline for search
+				command_palette = false, -- position the cmdline and popupmenu together
+				long_message_to_split = false, -- long messages will be sent to a split
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = true, -- add a border to hover docs and signature help
+			},
+		})
+	end,
 }
